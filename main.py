9eeca7c21507cfa698d04a7fec60a0d8e686847b
ASJoaoVictor <victor.sidiao@academico.ifpb.edu.br>
2025-05-16 13:52:27 +0000

    q3 a a8

--- main.py
+++ main.py
@@ -33,6 +33,15 @@ def q3():
     """
     Leia um número da entrada e imprima todos os 10 primeiros múltiplos dele na mesma linha
     """
+    num = int(input("Digite um número: "))
+
+    cont = 0
+    i = 1
+    while(cont < 10):
+        if(i % num == 0):
+            print(i, end=" ")
+            cont += 1
+        i+=1
     pass
 
 
@@ -45,7 +54,26 @@ def q4():
      - romulo junior - Romulo Junior
      - ze da manga - Ze da Manga
     """
+    nome_completo = input("Digite seu nome: ").split()
+    nome_atualizado = ""
+    for nome in nome_completo:
+        if(nome == "da" or nome =="de"):
+            nome_atualizado += f"{nome} "
+        else:
+            nome_atualizado += f"{nome[0].upper()}{nome[1:]} "
+    
+    print(nome_atualizado[0:len(nome_atualizado)-1])
     pass
+         
+"""     for i in range(0, len(nome_completo)):
+        if(nome_completo[i] == "de" or nome_completo[i] == "da"):
+            print(nome_completo[i], end="")
+        else:
+            print(f"{nome_completo[i][0].upper()}{nome_completo[i][1:]}", end="")
+
+        if(i != len(nome_completo)-1):
+            print(end=" ")
+    print("") """
 
 def q5():
     """
@@ -57,15 +85,63 @@ def q5():
         322: isosceles
         234: escaleno
     """
+    lados = input("Digite os valores dos lados: ")
+
+    if(lados[0] == lados[1] and lados[2] == lados[1]):
+        print("equilátero")
+    elif(lados[0] == lados[1] or lados[1] == lados[2] or lados[2] == lados[0]):
+        print("isósceles")
+    else:
+        print("escaleno")
+
     pass
 
 def q6():
+    """Periodicamente as crianças brasileiras devem tomar vacinas que as protegem de diversas doenças. 
+    Escrever um programa para ler o ano em que a criança toma a 1a dose e a periodicidade (intervalo em anos) da vacina e exibir em que outros anos a criança deve tomar as próximas doses desta vacina, sabendo que são 4(quatro) doses ao total.
+
+    Veja abaixo os exemplos de entrada e saída esperadas."""
+    ano = int(input("Digite o ano da primeira dose: "))
+    periodo = int(input("Digite periodicidade da vacina: "))
+
+    for i in range(0, 4):
+        ano += periodo
+        print(f"{ano}", end=" ")
     pass
 
 def q7():
+    """Faça um programa que leia uma sequencia de números e indique se eles são primos ou não.  Você deve parar ao ler o número -1."""
+
+    num = int(input("Digite um número: "))
+    while(num != -1):
+        controle = 0
+        for i in range(2, num+1):
+            if(num % i == 0):
+                controle += 1
+                
+        if(0 < controle < 2):
+            print("Primo")
+        else:
+            print("Não")
+
+        num = int(input("Digite um número: "))
     pass
 
 def q8():
+    """Valquíria trabalha em uma padaria e foi roubada ontem. Seus clientes ficaram com pena e resolveram organizar uma vaquinha para comprar um novo 
+    celular para ela. Escreva um programa que receba como entrada o valor doado por cada cliente, até que seja informado um valor negativo, 
+    e exiba o total arrecadado e o valor médio doado por eles."""
+
+    doacao = float(input("Digite o valor da doação: "))
+    total = 0
+    i = 0
+    media = 0
+    while(doacao >= 0):
+        i += 1
+        total += doacao
+        media = total/i
+        doacao = float(input("Digite o valro da doação: "))
+    print(f"{total:.2f}\n{(media):.2f}")
     pass
 
 def q9():
@@ -75,4 +151,4 @@ def q10():
     pass
 
 if __name__ == "__main__":
-    q2()
+    q7()
